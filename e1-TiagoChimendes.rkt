87afe63e2fa824288be04493da8246de0229c730
walescko <walescko@gmail.com>
2023-12-29 15:58:55 -0300

    rkt - final job

--- e1-TiagoChimendes.rkt
+++ e1-TiagoChimendes.rkt
@@ -10,14 +10,12 @@
 ;;===============================================================
 ;;constante MINHA-BANDEIRA:
 (define MINHA-BANDEIRA
-  (overlay (beside (triangle 30 "solid" "DarkBlue")
-          (above
-                  (text "Walescko" 16 "DarkBlue")
-                  (text "1903" 16 "DarkBlue")))
-           (above
-                  (rectangle 150 39 "solid" "RoyalBlue")
-                  (rectangle 150 42 "solid" "White")
-                  (rectangle 150 39 "solid" "Black"))))
+  (above                   
+                  (rectangle 150 60 "solid" "RoyalBlue")
+                  (overlay (beside
+                   (triangle 30 "solid" "DarkBlue")
+                   (text "1903" 20 "DarkBlue"))
+                  (rectangle 150 60 "solid" "DarkGrey"))))
 
 MINHA-BANDEIRA
 
@@ -33,25 +31,60 @@ MINHA-BANDEIRA
 
 ;(define (desenha-minha-bandeira ....)
 ;define colors
-(define colorWhite "white")
-(define colorDarkBlue "DarkBlue")
-(define colorBlack "black")
-(define colorRoyalBlue "RoyalBlue")
+(define color00 "RoyalBlue")
+(define color01 "DarkGrey")
+(define color02 "DarkBlue")
 ;define figures
-(define triangleMidle (triangle 30 "solid" colorDarkBlue))
-(define BarUp (rectangle 150 39 "solid" colorRoyalBlue))
-(define BarMidle (rectangle 150 42 "solid" colorWhite))
-(define BarDown (rectangle 150 39 "solid" colorBlack))
+(define BarUp (rectangle 150 60 "solid" color00))
+(define BarDown (rectangle 150 60 "solid" color01))
+(define triangleMidle (triangle 30 "solid" color02))
+(define (MyNumber choiceNumber)
+  (text (number->string choiceNumber) 20 color02))
+
+(define DESENHA-MINHA-BANDEIRA
+  (above
+     BarUp
+     (overlay
+      (beside 
+      triangleMidle (MyNumber 1903))
+     BarDown)))
+  
+
+DESENHA-MINHA-BANDEIRA
 
 
 ;;===============================================================
 ;; 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
 ;;===============================================================
 ;; ==> desenha-minha-bandeira-mais-generica
-;; desenha-minha-bandeira-mais-generica:  ...  -> ...
+;; desenha-minha-bandeira-mais-generica:  Cor barra do top, cor barra inferior, número, cor trianglo e número  -> imagem da bandeira
 ;; obj: ...
 ;; Exemplos:
-;;    (desenha-minha-bandeira-mais-generica ......) retorna ....
-;;    (desenha-minha-bandeira-mais-generica ......) retorna ....
-
+;;    (desenha-minha-bandeira-mais-generica "DodgerBlue" "Black" "1903" "White") retorna bandeira com barra superior na cor Dogde Blue, cor da barra inferior Black com o número 1903 e tringulo sobre a barra inferior na cor branca
 ;(define (desenha-minha-bandeira-mais-generica ....)
+
+(define (BarTop barTopColor)
+ (rectangle 150 60 "solid" barTopColor))
+
+(define (BarBooton barDownColor)
+ (rectangle 150 60 "solid" barDownColor))
+
+(define (triangleDown fontColor)
+ (triangle 30 "solid" fontColor))
+
+(define (MyNumberG choiceNumber color02)
+  (text (number->string choiceNumber) 20 color02))
+
+
+(define (desenha-minha-bandeira-mais-generica barTopColor barDownColor MyNumber fontColor )
+  (above
+     (BarTop barTopColor)
+     (overlay
+      (beside 
+      (triangleDown fontColor) (MyNumberG MyNumber fontColor))
+     (BarBooton barDownColor))))
+
+(desenha-minha-bandeira-mais-generica "DodgerBlue" "Black" 1903 "White")
+(desenha-minha-bandeira-mais-generica "LimeGreen" "PaleGoldenrod" 1983 "Lime")
+(desenha-minha-bandeira-mais-generica "Sea Green" "DarkViolet" 1995 "Dim Gray")
+(desenha-minha-bandeira-mais-generica "RoyalBlue" "DeepSkyBlue" 2017 "Dark Slate Blue")
