ee1859a2a8dd74974bb4347c285a50ee4efae693
walescko <walescko@gmail.com>
2024-01-07 08:10:23 -0300

    RKT - Exercício 2 - cards

---
+++ ex2-TiagoWalesckoChimendes.rkt
@@ -0,0 +1,32 @@
+#lang racket
+(require 2htdp/image)
+
+
+(define CIRCULO_BRANCO (circle 40 "solid""white"))
+
+(define QUADRADOS_COLORIDOS
+         (above                                  
+            (beside (rectangle 50 75 "solid" "red")
+                    (rectangle 50 75 "solid" "green"))
+            (beside (rectangle 50 75 "solid" "yellow")
+                    (rectangle 50 75 "solid" "blue"))))
+
+(define CONTORNO_PRETO (rectangle 110 160 "outline" "white"))
+
+(define CORINGA (overlay CIRCULO_BRANCO CONTORNO_PRETO QUADRADOS_COLORIDOS))
+
+(define AZUL (rectangle 100 150 "solid" "blue"))
+(define AMARELO (rectangle 100 150 "solid" "yellow"))
+(define VERDE (rectangle 100 150 "solid" "green"))
+(define VERMELHO (rectangle 100 150 "solid" "red"))
+
+(define CARTA_AZUL (overlay CIRCULO_BRANCO CONTORNO_PRETO AZUL))
+(define CARTA_AMARELO (overlay CIRCULO_BRANCO CONTORNO_PRETO AMARELO))
+(define CARTA_VERDE (overlay CIRCULO_BRANCO CONTORNO_PRETO VERDE))
+(define CARTA_VERMELHO (overlay CIRCULO_BRANCO CONTORNO_PRETO VERMELHO))
+
+(beside CORINGA
+CARTA_AZUL
+CARTA_AMARELO
+CARTA_VERDE
+CARTA_VERMELHO)
