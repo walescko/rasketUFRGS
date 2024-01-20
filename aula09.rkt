07113631e895312a47f74af1156279b731902ab8
walescko <walescko@gmail.com>
2024-01-20 19:07:08 -0300

    rkt - circle media)

--- aula09.rkt
+++ aula09.rkt
@@ -3,10 +3,18 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname aula09) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
 (define (mostra-media-estrela n)
   (overlay
-   (text (number->string (exact->inexact n)) 20 "black")
-   (star 60 "solid" "yellow"))) 
+   (text (number->string (numero1CasaDec n)) 20 "black")
+   (star 60 "solid" "yellow")))
 
+(define (mostra-media-circulo n)
+  (overlay
+   (text (number->string (numero1CasaDec n)) 20 "black")
+   (circle 60 "solid" "red"))) 
 
-(mostra-media-estrela 13)
+(define (numero1CasaDec n)
+  (exact->inexact (/ (round (* n 10)) 10)))
 
-(mostra-media-estrela 8.33)
+(mostra-media-estrela (/ 8 3))
+(mostra-media-estrela 8)
+(mostra-media-circulo 9)
+(mostra-media-circulo (/ 55 3))
