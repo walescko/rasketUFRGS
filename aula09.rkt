adf6f16a67ad0c6f94673ded41747987141c4bb8
walescko <walescko@gmail.com>
2024-01-20 17:58:26 -0300

    rkt - function overlay

---
+++ aula09.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname aula09) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+(define (mostra-media-estrela n)
+  (overlay
+   (text (number->string n) 20 "black")
+   (star 60 "solid" "yellow")))
+
+(mostra-media-estrela 44)
