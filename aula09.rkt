4933e2495fb60bb4e45f36bbb3a34778a92c35f9
walescko <walescko@gmail.com>
2024-01-24 15:02:45 -0300

    rkt - conditions

--- aula09.rkt
+++ aula09.rkt
@@ -18,3 +18,30 @@
 (mostra-media-estrela 8)
 (mostra-media-circulo 9)
 (mostra-media-circulo (/ 55 3))
+
+(define (is-five? num)
+  (= num 5))
+
+(is-five? 6)
+(is-five? 5)
+(is-five? 4)
+
+(define (between4-6? number)
+  (and
+   (< 4 number)
+   (> 6 number)))
+
+(between4-6? 3)
+(between4-6? 4)
+(between4-6? 5)
+(between4-6? 6)
+(between4-6? 7)
+
+
+
+  
+;Entrada: A média do aluno
+;Saída: mostra uma imagem com a média do aluno
+;Dada uma média de uma aluno, mostrar a média dentro de
+;uma estrela se ela for maior ou igual a 7, ou dentro de
+;um circulo vermelho caso contrário.
