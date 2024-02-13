e155d4387ade86e4054b089808c068b2ced43129
walescko <walescko@gmail.com>
2024-02-13 07:47:36 -0300

    rkt - number naipe card

--- aula11-12.rkt
+++ aula11-12.rkt
@@ -6,9 +6,30 @@
 ;;Se a pilha p está vazia, devolver false (não tem ás de copas)
 ;;Se o primeiro elemento da pilha p é às de copas, develver true
 ;;Senão, verificar se tem ás de copas no restante da pilha.
-(define (AsCopas? p)
-  (cond
-    [(empty? p) false]
-    [(AsCopas? (first p) true)]
-    [else (AsCopas? (rest p))]))
+;;(define (AsCopas? p)
+;;  (cond
+    ;;[(empty? p) false]
+  ;;  [(AsCopas? (first p) true)]
+;;    [else (AsCopas? (rest p))]))
 
+(define ÁsCopas 101)
+(define DezOuros 210)
+(define RainhaPaus 412)
+
+(define (numero-da-carta uma-carta)
+  (remainder uma-carta 100))
+
+(define (naipe-da-carta uma-carta)
+  (cond
+    [(= (quotient uma-carta 100) 1) "Copas" ]
+    [(= (quotient uma-carta 100) 2)"Ouros"]
+    [(= (quotient uma-carta 100) 3)"Espadas"]
+    [(= (quotient uma-carta 100) 4)"Paus"]))
+
+(check-expect (numero-da-carta DezOuros) 10)
+(check-expect (numero-da-carta ÁsCopas) 1)
+(check-expect (naipe-da-carta DezOuros) 10)
+(check-expect (naipe-da-carta ÁsCopas) 1)
+
+
+  
